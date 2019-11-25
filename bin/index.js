9680608c5c3121163be955b8f31069b740f6e8df
Spencer Whyte <spencerwhyte@gmail.com>
2019-11-24 19:51:30 -0500

    Added the ability to decrypt individual directories as needed

--- bin/index.js
+++ bin/index.js
@@ -5,7 +5,15 @@ const library = require('../lib/index.js');
 if (process.argv.length < 3) {
   console.log('\nProper uses:\n\n    secure-files encrypt\n\nOR\n\n    secure-files decrypt\n');
 } else if (process.argv[2] === 'encrypt') {
-  library.encryptAll();
+  if (process.argv[3]) {
+    library.encrypt(process.argv[3]);
+  } else {
+    library.encryptAll();
+  }
 } else if (process.argv[2] === 'decrypt') {
-  library.decryptAll();
+  if (process.argv[3]) {
+    library.encrypt(process.argv[3]);
+  } else {
+    library.decryptAll();
+  }
 }
